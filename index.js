40ffb71c65b23a05b1f02602be03133e5731a74c
aliefwijaya <alieffitriyanto92@gmail.com>
2022-05-29 10:51:57 +0700

    challenge-01-alieffw

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser ={
+    nama: "Monica",
+    gender: "Female",
+    age:17,
+    email: "monica@dingdong.com",
+    favoriteColor:["Yellow","Pink","White","Purple"],
+    isHavePet:true,
+    education:[
+    { name:"SD 01", city:"Jakarta",graduate: 2016 },
+    { name:"SMP 01", city:"Jakarta",graduate: 2019 },
+    { name:"SMA 03", city:"Tangerang" }],
+    favoriteRestaurant: ["Bento","Sushi","Pancake","Eggy","Tempura","Padang","Tteok"]
+};
+const secondUser = {
+    name:"Wendy",
+    gender:"Male",
+    age:23,
+    email:"wendy@dingdong.com",
+    favoriteColor:["Blue","Black","Grey"],
+    isHavePet:false,
+    education: [
+        { name:"SD 02", city:"Jakarta", graduate:2010 },
+        { name:"SMP 03", city:"Bogor", graduate:2013 },
+        { name:"SMA 01", city:"Surabaya", graduate:2016 },
+        { name:"Universitas Maju", city:"Tangerang", graduate:2010 },
+    ],
+    favoriteRestaurant: ["Tempura","Bento","Pancake","Padang","Katsu","Geprek","Eggy","Sushi"]
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
